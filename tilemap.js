9c34ff6a80d458e0a16c4d6ea451b5ad2794cce7
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-08-28 23:50:14 -0400

    updated the lib for the GLPixyp5js update

--- tilemap.js
+++ tilemap.js
@@ -42,7 +42,7 @@ class GLTilemap {
       let tilemapTex = new Texture.T2D(0, this.gl.R16UI, this.res[0], this.res[1], 1, 0, this.gl.RED_INTEGER, this.gl.UNSIGNED_SHORT, this.tilemapData, this.gl);
       tilemapTex.bind();
     
-      this.tilemap = new GLPixy(this.pos, this.size, tilemapTex, 1, this.fbo, true, this.rdr, this.canvas);
+      this.tilemap = new GLPixy(this.pos, this.size, tilemapTex, 1, this.fbo, true, true, this.rdr, this.canvas);
       this.tilemap.setInterpolation(this.gl.NEAREST, this.gl.NEAREST);
   }
 
@@ -64,7 +64,7 @@ class GLTilemap {
     let tilesTex = new Texture.T2D(0, this.gl.RGBA16UI, tilesList.length, 1, 2, 0, this.gl.RGBA_INTEGER, this.gl.UNSIGNED_SHORT, tilesData, this.gl);
     tilesTex.bind();
 
-    this.tiles = new GLPixy([0, 0], [0, 0], tilesTex, 1, this.fbo, true, this.rdr, this.canvas);
+    this.tiles = new GLPixy([0, 0], [0, 0], tilesTex, 1, this.fbo, true, true, this.rdr, this.canvas);
     this.tiles.setInterpolation(this.gl.NEAREST, this.gl.NEAREST);
   }
   
