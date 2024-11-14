bfaddcb5281bc3db6796335791d02be4511e7a42
randomgamingdev <randomgamingdev@randomgamingdev.randomgamingdev>
2024-11-14 11:03:59 -0500

    added extra comments describing how to use the library and what each parameter does this should be more than enough for typical use

--- examples/p5js-example/sketch.js
+++ examples/p5js-example/sketch.js
@@ -15,10 +15,25 @@ function setup() {
   
   gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
   gl.pixelStorei(gl.PACK_ALIGNMENT, 1);
-  tilemap = new GLTilemap([-200, -200], [400, 400], [3, 3], img, new Uint16Array([0, 1, 2, 2, 1, 0, 0, 1, 2]), [[0, 0, 1, 1], [0.5, 0.5, 0.5, 0.5]]);
+  tilemap = new GLTilemap(
+    [-width / 2, -height / 2], // pos: position of the tilemap from the top left
+    [width, height], // size: size of the tilemap extending extending from the top left
+    [3, 3], // res: of the tilemap in tiles
+    img, // img: the texture atlas image containing all of the textures to be used for the tilemap
+    new Uint16Array([ // tilemapData: The tiles stored via ints referencing their tiles from tilesList (goes from left to right then top to bottom)
+      0, 1, 2,
+      2, 1, 0,
+      0, 1, 2
+    ]),
+    [ // tilesList: List of tiles based on their positions in the texture atlas where top left is (0, 0), and bottom right is (1, 1)
+      [0, 0, 1, 1], // The first 2 numbers are the coordinates of the top left and the latter 2 numbers are the size of the tile's texture within the texture atlas
+      [0.5, 0.5, 0.5, 0.5]
+    ]
+  );
 }
 
 function draw() {
   background(220);
-  tilemap.display();
+
+  tilemap.display(); // Display the tilemap in a single draw call and with autmoatic tile occlusion :D
 }
