ba10205b22ddc83e284cc8bae4bd6aa95d62e625
RandomGamingDev <randomgamingdev@gmail.com>
2024-11-21 20:24:44 -0500

    fixed compatibility with p5.js versions for rendering before .display() on a frame

--- examples/p5js-example/tilemap.js
+++ examples/p5js-example/tilemap.js
@@ -31,7 +31,7 @@ class GLTilemap {
   }
   
   loadTilemap() {
-      let tilemapTex = new Texture.T2D(0, this.gl.R16UI, this.res[0], this.res[1], 1, 0, this.gl.RED_INTEGER, this.gl.UNSIGNED_SHORT, this.tilemapData, this.gl);
+      let tilemapTex = new Texture.T2D(0, this.gl.R16UI, this.res[0], this.res[1], 1, 0, this.gl.RED_INTEGER, this.gl.UNSIGNED_SHORT, this.tilemapData, this.gl); // slot 1
       tilemapTex.bind();
     
       this.tilemap = new GLPixy(this.pos, this.size, tilemapTex, 1, null, false, false, this.rdr, this.canvas);
@@ -53,7 +53,7 @@ class GLTilemap {
       }
     }
 
-    let tilesTex = new Texture.T2D(0, this.gl.RGBA16UI, tilesList.length, 1, 2, 0, this.gl.RGBA_INTEGER, this.gl.UNSIGNED_SHORT, tilesData, this.gl);
+    let tilesTex = new Texture.T2D(0, this.gl.RGBA16UI, tilesList.length, 1, 2, 0, this.gl.RGBA_INTEGER, this.gl.UNSIGNED_SHORT, tilesData, this.gl); // slot 2
     tilesTex.bind();
 
     this.tiles = new GLPixy([0, 0], [0, 0], tilesTex, 1, null, false, false, this.rdr, this.canvas);
@@ -68,15 +68,15 @@ class GLTilemap {
 
       this.gl.activeTexture(this.gl.TEXTURE0);
       this.gl.bindTexture(this.gl.TEXTURE_2D, this.img.texture.glTex);
-      this.gl.uniform1i(this.gl.getUniformLocation(this.shad._glProgram, "atlas"), 0);
+      this.shad.setUniform("atlas", 0);
       
       this.tilemap.img.activate();
       this.tilemap.img.bind();
-      this.tilemap.img.setUniform(this.shad._glProgram, "uSampler");
+      this.shad.setUniform("tilemap", 1);
 
       this.tiles.img.activate();
       this.tiles.img.bind();
-      this.tiles.img.setUniform(this.shad._glProgram, "tiles");
+      this.shad.setUniform("tiles", 2);
 
       this.rdr.rect(this.tilemap.off[0], this.tilemap.off[1], this.tilemap.size[0], this.tilemap.size[1]);
 
@@ -112,13 +112,13 @@ GLTilemap.fragShader = `#version 300 es
   in vec2 vTexCoord;
 
   uniform sampler2D atlas;
-  uniform mediump usampler2D uSampler;
+  uniform mediump usampler2D tilemap;
   uniform mediump usampler2D tiles;
 
   out vec4 fragColor;
 
   void main() {
-    uint tile = texture(uSampler, vTexCoord).r;
+    uint tile = texture(tilemap, vTexCoord).r;
 
     if (tile == uint(0)) {
       fragColor = vec4(0.0);
@@ -129,10 +129,135 @@ GLTilemap.fragShader = `#version 300 es
     uvec4 iCoords = texelFetch(tiles, ivec2(tile - uint(1), 0), 0);
     vec4 fCoords = vec4(iCoords) / float(MAX_UNSIGNED_SHORT);
 
-    vec2 tilemapSize = vec2(textureSize(uSampler, 0));
+    vec2 tilemapSize = vec2(textureSize(tilemap, 0));
     vec2 tileSize = 1.0 / tilemapSize;
     vec2 tileTexCoord = mod(vTexCoord, tileSize) * tilemapSize;
 
     fragColor = texture(atlas, fCoords.rg + tileTexCoord * fCoords.ba);
   }
 `;
+
+// Overwrite p5.js setUniform function to work with different data formats
+p5.Shader.prototype.setUniform = function (uniformName, data) {
+  const uniform = this.uniforms[uniformName];
+  if (!uniform) {
+    return;
+  }
+
+  const gl = this._renderer.GL;
+
+  if (uniform.isArray) {
+    if (
+      uniform._cachedData &&
+      this._renderer._arraysEqual(uniform._cachedData, data)
+    ) {
+      return;
+    } else {
+      uniform._cachedData = data.slice(0);
+    }
+  } else if (uniform._cachedData && uniform._cachedData === data) {
+    return;
+  } else {
+    if (Array.isArray(data)) {
+      uniform._cachedData = data.slice(0);
+    } else {
+      uniform._cachedData = data;
+    }
+  }
+
+  const location = uniform.location;
+
+  this.useProgram();
+  
+  switch (uniform.type) {
+    case gl.BOOL:
+      if (data === true) {
+        gl.uniform1i(location, 1);
+      } else {
+        gl.uniform1i(location, 0);
+      }
+      break;
+    case gl.INT:
+      if (uniform.size > 1) {
+        data.length && gl.uniform1iv(location, data);
+      } else {
+        gl.uniform1i(location, data);
+      }
+      break;
+    case gl.FLOAT:
+      if (uniform.size > 1) {
+        data.length && gl.uniform1fv(location, data);
+      } else {
+        gl.uniform1f(location, data);
+      }
+      break;
+    case gl.FLOAT_MAT3:
+      gl.uniformMatrix3fv(location, false, data);
+      break;
+    case gl.FLOAT_MAT4:
+      gl.uniformMatrix4fv(location, false, data);
+      break;
+    case gl.FLOAT_VEC2:
+      if (uniform.size > 1) {
+        data.length && gl.uniform2fv(location, data);
+      } else {
+        gl.uniform2f(location, data[0], data[1]);
+      }
+      break;
+    case gl.FLOAT_VEC3:
+      if (uniform.size > 1) {
+        data.length && gl.uniform3fv(location, data);
+      } else {
+        gl.uniform3f(location, data[0], data[1], data[2]);
+      }
+      break;
+    case gl.FLOAT_VEC4:
+      if (uniform.size > 1) {
+        data.length && gl.uniform4fv(location, data);
+      } else {
+        gl.uniform4f(location, data[0], data[1], data[2], data[3]);
+      }
+      break;
+    case gl.INT_VEC2:
+      if (uniform.size > 1) {
+        data.length && gl.uniform2iv(location, data);
+      } else {
+        gl.uniform2i(location, data[0], data[1]);
+      }
+      break;
+    case gl.INT_VEC3:
+      if (uniform.size > 1) {
+        data.length && gl.uniform3iv(location, data);
+      } else {
+        gl.uniform3i(location, data[0], data[1], data[2]);
+      }
+      break;
+    case gl.INT_VEC4:
+      if (uniform.size > 1) {
+        data.length && gl.uniform4iv(location, data);
+      } else {
+        gl.uniform4i(location, data[0], data[1], data[2], data[3]);
+      }
+      break;
+    case gl.SAMPLER_2D:
+      if (typeof data == 'number') {
+        gl.activeTexture(gl.TEXTURE0 + data);
+        gl.uniform1i(location, data);
+      }
+      else {
+        gl.activeTexture(gl.TEXTURE0 + uniform.samplerIndex);
+        uniform.texture = data instanceof p5.Texture ? data : this._renderer.getTexture(data);
+        gl.uniform1i(location, uniform.samplerIndex);
+        if (typeof uniform.texture === 'object' && uniform.texture.src.gifProperties) {
+          uniform.texture.src._animateGif(this._renderer._pInst);
+        }
+      }
+      break;
+    case gl.UNSIGNED_INT_SAMPLER_2D:
+      gl.activeTexture(gl.TEXTURE0 + data);
+      gl.uniform1i(location, data);
+      break;
+    //@todo complete all types
+  }
+  return this;
+}
